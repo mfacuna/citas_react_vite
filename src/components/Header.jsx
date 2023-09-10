28d9af3f0a69baf9463981fd5aab1e805205c202
devmata <mfacuna@ing.ucsc.cl>
2023-09-10 13:03:58 -0300

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({}) => {
 
 
   return (
-            <h1 className=" font-black text-xl border-b-4 pb-2 border-indigo-600 md:w-auto">
+            <h1 className=" font-black text-xl border-b-2 pb-5 pt-5 border-indigo-600 md:w-auto text-center">
               Seguimiento Pacientes {''}
               <span className="text-indigo-600">Veterinaria</span>
             </h1>
